e02082c2a6d29acc85b8c56f8fa4a7b78e2c38b9
Anannya K <142819550+ahsplore@users.noreply.github.com>
2023-10-04 21:56:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var image1 = "images/dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src", image1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var image2 = "images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", image2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector(".container h1").textContent = "🚩Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector(".container h1").textContent = "Player 2 Wins!🚩";
+} else {
+    document.querySelector(".container h1").textContent = "Draw!";
+}
